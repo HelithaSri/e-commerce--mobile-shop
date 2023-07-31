ef1c5d35bd1d98601380abcb52f4f3357485ad48
HelithaSri <helitha.pravin@gmail.com>
2023-08-01 01:57:48 +0530

    quick fixed

--- backend/service/cartService.js
+++ backend/service/cartService.js
@@ -14,12 +14,8 @@ const getCartItems = async () => {
 
 // Add item to cart
 const addToCart = async (productId, quantity) => {
-    /*const filters = { productId: productId };
-    const filter = { items: filters };*/
-
     try {
         const cartItem = await Cart.findOne({'items.productId': productId});
-        console.log(cartItem)
 
         if (cartItem != null) {
             // If the item already exists in the cart, update the quantity
@@ -41,7 +37,7 @@ const addToCart = async (productId, quantity) => {
             const newCartItem = new Cart(cartData);
             newCartItem.save()
                 .then(savedCart => {
-                    console.log('Cart saved successfully:', savedCart);
+                    console.log('Cart saved successfully:');
                 })
                 .catch(error => {
                     console.error('Error saving cart:', error);
@@ -58,7 +54,11 @@ const addToCart = async (productId, quantity) => {
 // Remove item from cart
 const removeFromCart = async (productId) => {
     try {
-        await Cart.deleteOne({ 'items.productId': productId });
+        let deletedItem = await Cart.deleteOne({ 'items.productId': productId });
+        if (deletedItem.deletedCount === 0){
+            throw new Error("Item not found.");
+        }
+        console.log("Item removed from the cart")
         return;
     } catch (error) {
         console.error("Error --> removeFromCart ", error);
